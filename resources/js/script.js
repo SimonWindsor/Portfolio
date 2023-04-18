627cbb98142e8f5d2799874c2a4c8ae2d0e81b1b
Simon Windsor <s3918370@student.rmit.edu.au>
2023-04-19 00:36:33 +1000

    added an animation to menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,11 +2,12 @@ const hamburgerIcon = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
 
 hamburgerIcon.addEventListener('click', () => {
-  if (menu.classList.contains("closed")) {
-    menu.classList.remove("closed");
-    menu.classList.add("open");
-  } else if (menu.classList.contains("open")) {
-    menu.classList.remove("open");
-    menu.classList.add("closed");
+  menu.classList.remove('initial');
+  if (menu.classList.contains('closed')) {
+    menu.classList.remove('closed');
+    menu.classList.add('open');
+  } else if (menu.classList.contains('open')) {
+    menu.classList.remove('open');
+    menu.classList.add('closed');
   }
 })
