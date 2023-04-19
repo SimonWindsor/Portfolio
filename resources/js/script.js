e63ab10885dd091d3767adcba2712d13e5e9e4db
Simon Windsor <s3918370@student.rmit.edu.au>
2023-04-19 22:08:05 +1000

    replaced hamburger menu with top menu, animation included

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,13 +1,1 @@
-const hamburgerIcon = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-
-hamburgerIcon.addEventListener('click', () => {
-  menu.classList.remove('initial');
-  if (menu.classList.contains('closed')) {
-    menu.classList.remove('closed');
-    menu.classList.add('open');
-  } else if (menu.classList.contains('open')) {
-    menu.classList.remove('open');
-    menu.classList.add('closed');
-  }
-})
+const dropDowns = document.getElementsByClassName('dropdown');
