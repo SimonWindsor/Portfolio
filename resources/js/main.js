b66e64a2a088de10dc30b0172c48e341af4b3a3d
Simon <simon.nicholas@outlook.com>
2024-07-12 17:13:22 +1000

    project pages for mobile more funcitonal. Beginning menu for mobile adjustments

---
+++ resources/js/main.js
@@ -0,0 +1,6 @@
+// Opens hamburger menu in portrait mode
+function showMenu() {
+  const menu = document.getElementById('menu');
+  
+  menu.style.display = menu.style.display === '' ? 'flex' : '';
+}
