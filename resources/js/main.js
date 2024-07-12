8a24099a9a88cd13d56835c93ef1c61c76ae30fc
Simon <simon.nicholas@outlook.com>
2024-07-12 23:06:37 +1000

    Hamburger menu seems finished

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,35 @@
-// Opens hamburger menu in portrait mode
+// Opens hamburger menu in portrait mode.
 function showMenu() {
   const menu = document.getElementById('menu');
-  
   menu.style.display = menu.style.display === '' ? 'flex' : '';
+}
+
+// Changes display of "Projects" link in nav bar according to mobile or large display.
+function changeProjectMenu() {
+  const projectsLink = document.getElementById('projects-link');
+  const chevron = document.getElementById('chevron');
+
+  if (screen.height > screen.width) {
+    projectsLink.innerHTML = 'Projects';
+    chevron.style.display = 'block';
+  } else if (screen.height < screen.width) {
+    projectsLink.innerHTML = 'Projects ˅';
+    chevron.style.display = '';
+  }
+}
+
+// For mobile screen, opens project sub-menu as accordian menu
+function openAccordian() {
+  const projectsMenu = document.getElementsByClassName('dropdown')[0];
+  const chevron = document.getElementById('chevron');
+  const skills = document.getElementById('skills-link');
+  const contact = document.getElementById('contact-link');
+
+  if (projectsMenu.classList.contains('accordian')) {
+    projectsMenu.classList.remove('accordian');
+    chevron.style.transform = 'rotateX(0)';
+  } else {
+    projectsMenu.classList.add('accordian');
+    chevron.style.transform = 'rotateX(180deg)';
+  }
 }
